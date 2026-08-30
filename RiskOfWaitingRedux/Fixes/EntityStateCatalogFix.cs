fbd42c4216b58ec32ea615cdbd99e4c85ef4d1e2
Groove Salad <95263024+Priscillalala@users.noreply.github.com>
2026-08-30 11:56:13 -0700

    update comment

--- RiskOfWaitingRedux/Fixes/EntityStateCatalogFix.cs
+++ RiskOfWaitingRedux/Fixes/EntityStateCatalogFix.cs
@@ -18,7 +18,7 @@ public static class EntityStateCatalogFix
     private static IEnumerator SetElementsNoYielding(IEnumerator result)
     {
         // yield one or two times to maintain a consistent framerate
-        // this is the equivalent of yielding after every 600 configurations are applied, since the original coroutine already yields 1 in 10
+        // this is the equivalent of yielding after every 800 configurations are applied, since the original coroutine already yields 1 in 10
         const int MAX_YIELD_ATTEMPTS_PER_FRAME = 80;
         int yieldAttemptsThisFrame = 0;
         while (result.MoveNext())
